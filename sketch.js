db8a0097321d1f7eac2816db5f02ae7f8166e1d4
Felipesch07 <144177385+felipeschmidt0707@users.noreply.github.com>
2024-09-23 10:08:16 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+}
+
+function draw() {
+  
+  noStroke();
+  fill("#2196F3")
+  rect(0, 0, 400);
+  
+  noStroke();
+  fill("rgb(0,0,63)")
+  rect(0, 200, 400)
+   
+  noStroke();
+  fill("yellow")
+  circle(220, 100, 80)
+}
